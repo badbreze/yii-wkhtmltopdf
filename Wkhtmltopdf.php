7ec2f7879f8518431dfbcd772e18992309265602
Damian Gomez <racksoft@gmail.com>
2014-12-04 11:06:45 +0100

    Removed binary & verify OS

--- Wkhtmltopdf.php
+++ Wkhtmltopdf.php
@@ -2,163 +2,168 @@
 
 class Wkhtmltopdf extends WkhtpObject
 {
-	/** @var string	NULL means autodetect */
-	public static $executable;
 
-	/** @var array	possible executables */
-	public static $executables = array(
-        'wkhtmltopdf-i386',
-        'wkhtmltopdf-osx',
-        'wkhtmltopdf-amd64');
-	/** @var int */
-	public $dpi = 300;
+    /** @var string	NULL means autodetect */
+    public static $executable;
 
-	/** @var array */
-	public $margin = array(0,0,0,0);
+    /** @var array	possible executables */
+    public static $executables = array('wkhtmltopdf');
 
-	/** @var string */
-	public $orientation = 'portrait';
+    /** @var array	possible executables */
+    public static $executable_paths = array(
+        '/bin',
+        '/usr/bin'
+    );
 
-	/** @var string */
-	public $size = 'A4';
+    /** @var int */
+    public $dpi = 300;
 
-	/** @var string */
-	public $title;
+    /** @var array */
+    public $margin = array(0, 0, 0, 0);
 
-	/** @var string */
-	public $encoding = 'utf-8';
+    /** @var string */
+    public $orientation = 'portrait';
 
-	/** @var bool */
-	public $usePrintMediaType = TRUE;
+    /** @var string */
+    public $size = 'A4';
 
-	/** @var string */
-	public $styleSheet;
+    /** @var string */
+    public $title;
 
-	/** @var PageMeta */
-	protected $header;
+    /** @var string */
+    public $encoding = 'utf-8';
 
-	/** @var PageMeta */
-	protected $footer;
+    /** @var bool */
+    public $usePrintMediaType = TRUE;
 
-	/** @var array */
-	protected $pages = array();
+    /** @var string */
+    public $styleSheet;
 
-	/** @var string */
-	public $tmpDir;
+    /** @var PageMeta */
+    protected $header;
 
-	/** @var array */
-	protected $tmpFiles = array();
+    /** @var PageMeta */
+    protected $footer;
 
-	/** @var resource */
-	protected $p;
+    /** @var array */
+    protected $pages = array();
 
-	/** @var array */
-	protected $pipes;
+    /** @var string */
+    public $tmpDir;
 
-	/**
-	 * @param string
-	 */
-	public function __construct($tmpDir)
-	{
+    /** @var array */
+    protected $tmpFiles = array();
+
+    /** @var resource */
+    protected $p;
+
+    /** @var array */
+    protected $pipes;
+
+    /**
+     * @param string
+     */
+    public function __construct($tmpDir)
+    {
         //putenv("FONTCONFIG_PATH=/home/emanage/etc/fonts");
-		$this->tmpDir = $tmpDir;
-	}
+        $this->tmpDir = $tmpDir;
+    }
 
-	/**
-	 * @return PageMeta
-	 */
-	public function getHeader()
-	{
-		if ($this->header === NULL) {
-			$this->header = new WkhtpPageMeta('header');
-		}
-		return $this->header;
-	}
+    /**
+     * @return PageMeta
+     */
+    public function getHeader()
+    {
+        if ($this->header === NULL) {
+            $this->header = new WkhtpPageMeta('header');
+        }
+        return $this->header;
+    }
 
-	/**
-	 * @return PageMeta
-	 */
-	public function getFooter()
-	{
-		if ($this->footer === NULL) {
-			$this->footer = new WkhtpPageMeta('footer');
-		}
-		return $this->footer;
-	}
+    /**
+     * @return PageMeta
+     */
+    public function getFooter()
+    {
+        if ($this->footer === NULL) {
+            $this->footer = new WkhtpPageMeta('footer');
+        }
+        return $this->footer;
+    }
 
-	/**
-	 * @param  string
-	 * @param  bool
-	 * @return Page
-	 */
-	public function addHtml($html, $isCover = FALSE)
-	{
-		$this->pages[] = $page = $this->createPage();
-		$page->html = $html;
-		$page->isCover = $isCover;
-		return $page;
-	}
+    /**
+     * @param  string
+     * @param  bool
+     * @return Page
+     */
+    public function addHtml($html, $isCover = FALSE)
+    {
+        $this->pages[] = $page = $this->createPage();
+        $page->html = $html;
+        $page->isCover = $isCover;
+        return $page;
+    }
 
-	/**
-	 * @param string
-	 * @param bool
-	 * @return Page
-	 */
-	public function addFile($file, $isCover = FALSE)
-	{
-		$this->pages[] = $page = $this->createPage();
-		$page->file = $file;
-		$page->isCover = $isCover;
-		return $page;
-	}
+    /**
+     * @param string
+     * @param bool
+     * @return Page
+     */
+    public function addFile($file, $isCover = FALSE)
+    {
+        $this->pages[] = $page = $this->createPage();
+        $page->file = $file;
+        $page->isCover = $isCover;
+        return $page;
+    }
 
-	/**
-	 * @param  string
-	 * @return Toc
-	 */
-	public function addToc($header = NULL)
-	{
-		$this->pages[] = $toc = new WkhtpToc;
-		if ($header !== NULL) {
-			$toc->header = $header;
-		}
-		return $toc;
-	}
+    /**
+     * @param  string
+     * @return Toc
+     */
+    public function addToc($header = NULL)
+    {
+        $this->pages[] = $toc = new WkhtpToc;
+        if ($header !== NULL) {
+            $toc->header = $header;
+        }
+        return $toc;
+    }
 
-	/**
-	 * @param  IDocumentPart
-	 * @return Document
-	 */
-	public function addPart(WkhtpDocumentPart $part)
-	{
-		$this->pages[] = $part;
-		return $this;
-	}
+    /**
+     * @param  IDocumentPart
+     * @return Document
+     */
+    public function addPart(WkhtpDocumentPart $part)
+    {
+        $this->pages[] = $part;
+        return $this;
+    }
 
-	/**
-	 * @return Page
-	 */
-	private function createPage()
-	{
-		$page = new WkhtpPage;
-		$page->encoding = $this->encoding;
-		$page->usePrintMediaType = $this->usePrintMediaType;
-		$page->styleSheet = $this->styleSheet;
-		return $page;
-	}
+    /**
+     * @return Page
+     */
+    private function createPage()
+    {
+        $page = new WkhtpPage;
+        $page->encoding = $this->encoding;
+        $page->usePrintMediaType = $this->usePrintMediaType;
+        $page->styleSheet = $this->styleSheet;
+        return $page;
+    }
 
-	/**
-	 * @param  string
-	 * @return string
-	 */
-	public function saveTempFile($content)
-	{
-		do {
-			$file = $this->tmpDir . '/' . md5($content . '.' . lcg_value()) . '.html';
-		} while (file_exists($file));
-		file_put_contents($file, $content);
-		return $this->tmpFiles[] = $file;
-	}
+    /**
+     * @param  string
+     * @return string
+     */
+    public function saveTempFile($content)
+    {
+        do {
+            $file = $this->tmpDir . '/' . md5($content . '.' . lcg_value()) . '.html';
+        } while (file_exists($file));
+        file_put_contents($file, $content);
+        return $this->tmpFiles[] = $file;
+    }
 
     public function headersTransfer($filename, $length)
     {
@@ -174,13 +179,13 @@ class Wkhtmltopdf extends WkhtpObject
         header("Content-Type: application/octec-stream", false);
         header("Content-Type: application/download", false);
         header("Content-Type: application/pdf", false);
-        header("Content-Length: ". $length);
-        header('Content-Disposition: attachment; filename="' . basename($filename) .'";');
-        header("Content-Transfer-Encoding: binary"); 
+        header("Content-Length: " . $length);
+        header('Content-Disposition: attachment; filename="' . basename($filename) . '";');
+        header("Content-Transfer-Encoding: binary");
     }
 
     public function headersEmbed($filename, $length)
-	{
+    {
         if (headers_sent()) {
             throw new WkhtpException("Headers already sent");
         }
@@ -190,18 +195,19 @@ class Wkhtmltopdf extends WkhtpObject
         header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
         header("Last-Modified: " . gmdate('D, d m Y H:i:s') . " GMT");
         header("Content-Length: " . $length);
-        header('Content-Disposition: inline; filename="' . basename($filename) .'";');
+        header('Content-Disposition: inline; filename="' . basename($filename) . '";');
     }
-	/**
-	 * Send headers and outputs PDF document to browser.
-	 * @throws WkhtpException
-	 */
-	public function send($filename = 'file.pdf', $embed = false)
-	{
-		$this->convert();
-		$output = fgets($this->pipes[1], 5);
-        $meta   = stream_get_meta_data($this->pipes[1]);
-		if ($output === '%PDF') {
+
+    /**
+     * Send headers and outputs PDF document to browser.
+     * @throws WkhtpException
+     */
+    public function send($filename = 'file.pdf', $embed = false)
+    {
+        $this->convert();
+        $output = fgets($this->pipes[1], 5);
+        $meta = stream_get_meta_data($this->pipes[1]);
+        if ($output === '%PDF') {
             ob_start();
             echo $output;
             fpassthru($this->pipes[1]);
@@ -214,114 +220,127 @@ class Wkhtmltopdf extends WkhtpObject
             ob_end_flush();
             ob_flush();
             flush();
-		}
-		$this->close();
-	}
+        }
+        $this->close();
+    }
 
-	/**
-	 * Save PDF document to file.
-	 * @param  string
-	 * @throws WkhtpException
-	 */
-	public function save($file)
-	{
-		$f = fopen($file, 'w');
-		$this->convert();
-		stream_copy_to_stream($this->pipes[1], $f);
-		fclose($f);
-		$this->close();
-	}
+    /**
+     * Save PDF document to file.
+     * @param  string
+     * @throws WkhtpException
+     */
+    public function save($file)
+    {
+        $f = fopen($file, 'w');
+        $this->convert();
+        stream_copy_to_stream($this->pipes[1], $f);
+        fclose($f);
+        $this->close();
+    }
 
-	/**
-	 * Returns PDF document as string.
-	 * @return string
-	 */
-	public function __toString()
-	{
-		try {
-			$this->convert();
-			$s = stream_get_contents($this->pipes[1]);
-			$this->close();
-			return $s;
-		} catch (\Exception $e) {
-			trigger_error($e->getMessage(), E_USER_ERROR);
-		}
-	}
+    /**
+     * Returns PDF document as string.
+     * @return string
+     */
+    public function __toString()
+    {
+        try {
+            $this->convert();
+            $s = stream_get_contents($this->pipes[1]);
+            $this->close();
+            return $s;
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_ERROR);
+        }
+    }
 
-	protected function convert()
-	{
-		if (self::$executable === NULL) {
-			self::$executable = $this->detectExecutable();
-		}
+    protected function convert()
+    {
+        if (self::$executable === NULL) {
+            self::$executable = $this->detectExecutable();
+        }
 
-		if (self::$executable === FALSE) {
-			throw new WkhtpException('Cannot found Wkhtmltopdf executable');
-		}
+        if (self::$executable === FALSE) {
+            throw new WkhtpException('Cannot found Wkhtmltopdf executable');
+        }
 
-		$m = $this->margin;
-		$cmd = self::$executable . ' -q '
-        // --no-header-line --no-footer-line --disable-smart-shrinking --disable-internal-links
-			. ' -T ' . escapeshellarg($m[0])
-			. ' -R ' . escapeshellarg($m[1])
-			. ' -B ' . escapeshellarg($m[2])
-			. ' -L ' . escapeshellarg($m[3])
-			. ' --dpi ' . escapeshellarg($this->dpi)
-			. ' --page-size ' . escapeshellarg($this->size)
-			. ' --orientation ' . escapeshellarg($this->orientation)
-			. ' --title ' . escapeshellarg($this->title);
+        $m = $this->margin;
+        $cmd = self::$executable . ' -q '
+                // --no-header-line --no-footer-line --disable-smart-shrinking --disable-internal-links
+                . ' -T ' . escapeshellarg($m[0])
+                . ' -R ' . escapeshellarg($m[1])
+                . ' -B ' . escapeshellarg($m[2])
+                . ' -L ' . escapeshellarg($m[3])
+                . ' --dpi ' . escapeshellarg($this->dpi)
+                . ' --page-size ' . escapeshellarg($this->size)
+                . ' --orientation ' . escapeshellarg($this->orientation)
+                . ' --title ' . escapeshellarg($this->title);
 
-		if ($this->header !== NULL) {
-			$cmd .= ' ' . $this->header->buildShellArgs($this);
-		}
-		if ($this->footer !== NULL) {
-			$cmd .= ' ' . $this->footer->buildShellArgs($this);
-		}
-		foreach ($this->pages as $page) {
-			$cmd .= ' ' . $page->buildShellArgs($this);
-		}
+        if ($this->header !== NULL) {
+            $cmd .= ' ' . $this->header->buildShellArgs($this);
+        }
+        if ($this->footer !== NULL) {
+            $cmd .= ' ' . $this->footer->buildShellArgs($this);
+        }
+        foreach ($this->pages as $page) {
+            $cmd .= ' ' . $page->buildShellArgs($this);
+        }
 
-		$this->p = $this->openProcess($cmd . ' -', $this->pipes);
-	}
+        $this->p = $this->openProcess($cmd . ' -', $this->pipes);
+    }
 
-	/**
-	 * Returns path to executable.
-	 * @return string
-	 */
-	protected function detectExecutable()
-	{
-        $path = realpath( dirname(__FILE__) . DIRECTORY_SEPARATOR . 'bin');
+    /**
+     * Returns path to executable.
+     * @return string
+     */
+    protected function detectExecutable()
+    {
+        $os = strtolower(substr(php_uname('s'), 0, 3));
 
-		foreach (self::$executables as $exec) {
-            $exec = $path . DIRECTORY_SEPARATOR . $exec;
-			if (proc_close($this->openProcess("$exec -v", $tmp)) === 1) {
-				return $exec;
-			}
-		}
+        if ($os == 'win') {
+            return 'wkhtmltopdf.exe';
+        } else {
+            foreach (self::$executables as $exec) {
+                if (proc_close($this->openProcess("$exec -v", $tmp)) === 1) {
+                    return $exec;
+                }
+
+                foreach (self::$executable_paths as $path) {
+                    $exec = $path . DIRECTORY_SEPARATOR . $exec;
+                    if (proc_close($this->openProcess("$exec -v", $tmp)) === 1) {
+                        return $exec;
+                    }
+                }
+            }
+        }
+        
         return FALSE;
-	}
+    }
 
-	protected function openProcess($cmd, & $pipes)
-	{
-		static $spec = array(
-			1 => array('pipe', 'w'),
-			2 => array('pipe', 'w'),
-		);
-		return proc_open($cmd, $spec, $pipes);
-	}
+    protected function openProcess($cmd, & $pipes)
+    {
+        static $spec = array(
+            1 => array('pipe', 'w'),
+            2 => array('pipe', 'w'),
+        );
+        return proc_open($cmd, $spec, $pipes);
+    }
 
-	protected function close()
-	{
-		stream_get_contents($this->pipes[1]); // wait for process
-		$error = stream_get_contents($this->pipes[2]);
-		if (proc_close($this->p) > 0) {
-			throw new WkhtpException($error);
-		}
-		foreach ($this->tmpFiles as $file) {
-			unlink($file);
-		}
-	}
-    
-    public function getPages() {
+    protected function close()
+    {
+        stream_get_contents($this->pipes[1]); // wait for process
+        $error = stream_get_contents($this->pipes[2]);
+        if (proc_close($this->p) > 0) {
+            throw new WkhtpException($error);
+        }
+        foreach ($this->tmpFiles as $file) {
+            unlink($file);
+        }
+    }
+
+    public function getPages()
+    {
         return $this->pages;
     }
+
 }
